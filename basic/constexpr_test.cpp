184856fae06dda70007501af5f1d5ef13881246b
icejoywoo <icejoywoo@gmail.com>
2022-07-16 15:03:28 +0800

    update

--- basic/constexpr_test.cpp
+++ basic/constexpr_test.cpp
@@ -79,3 +79,78 @@ TEST(ConstexprSuite, DoubleTest) {
     constexpr double s = sum(5.0);
     SIMPLE_PRINT(s, "%f");
 }
+
+constexpr int foo() {
+    return []() { return 58; } ();
+}
+
+TEST(ConstexprSuite, LambdaTest) {
+    char buffer1[foo()] = {0};
+    ASSERT_ARRAY_SIZE_AND_VALUE(buffer1, foo(), 0);
+    auto get_size = [](int i) { return i * 2; };
+    char buffer2[get_size(5)] = {0};
+    ASSERT_ARRAY_SIZE_AND_VALUE(buffer2, get_size(5), 0);
+
+    // functor
+    class GetSize {
+    public:
+        constexpr int operator()(int i) const {
+            return i * 2;
+        }
+    };
+    auto functor = GetSize();
+    char buffer3[functor(5)] = {0};
+    ASSERT_ARRAY_SIZE_AND_VALUE(buffer3, functor(5), 0);
+}
+
+TEST(ConstexprSuite, LambdaTest2) {
+    auto get_size = [](int i) constexpr -> int { return i * 2; };
+    char buffer1[get_size(5)] = {0};
+    ASSERT_ARRAY_SIZE_AND_VALUE(buffer1, get_size(5), 0);
+
+//    auto get_count = []() constexpr -> int {
+//        static int x = 5; // compile error
+//        return x;
+//    };
+}
+
+void check() {
+    if constexpr (sizeof(int) > sizeof(char)) {
+        printf("sizeof(int) > sizeof(char)\n");
+    } else {
+        printf("sizeof(int) <= sizeof(char)\n");
+    }
+}
+
+//template<class T> bool is_same_value(T a, T b) {
+//    return a == b;
+//}
+//
+//// specialization
+//template<> bool is_same_value(double a, double b) {
+//    return std::abs(a - b) < 0.0001;
+//}
+
+// if constexpr usage in template
+template<class T> bool is_same_value(T a, T b) {
+    if constexpr (std::is_same<T, double>::value) {
+        return std::abs(a - b) < 0.0001;
+    }
+    else {
+        return a == b;
+    }
+}
+
+TEST(ConstexprSuite, IfConstexprTest) {
+    check();
+
+    double x = 0.1 + 0.1 + 0.1 - 0.3;
+
+    SIMPLE_PRINT(is_same_value(5, 5), "%d");
+    ASSERT_TRUE(is_same_value(5, 5));
+    // floating point equality problem: its binary format lose some precision
+    SIMPLE_PRINT(x == 0.0, "%d");
+    ASSERT_FALSE(x == 0.0);
+    SIMPLE_PRINT(is_same_value(x, 0.0), "%d");
+    ASSERT_TRUE(is_same_value(x, 0.0));
+}
