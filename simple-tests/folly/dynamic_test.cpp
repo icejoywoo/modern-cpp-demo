51c39801ef39aec68f85bf1e2e5a03a244cf2912
icejoywoo <icejoywoo@gmail.com>
2022-07-26 21:40:51 +0800

    add dynamic test

---
+++ simple-tests/folly/dynamic_test.cpp
@@ -0,0 +1,74 @@
+#include "gtest/gtest.h"
+
+#include "folly/dynamic.h"
+#include "folly/json.h"
+
+/**
+ * folly::dynamic supported types:
+ * enum Type {
+ *   NULLT,    // nullptr_t
+ *   ARRAY,    // std::vector<folly::dynamic>
+ *   BOOL,     // bool
+ *   DOUBLE,   // double
+ *   INT64,    // int64_t
+ *   OBJECT,   // 类似于 std::unordered_map<folly::dynamic, folly::dynamic>
+ *   STRING,   // std::string
+ * };
+ *
+ * https://zhiqiang.org/coding/folly-dynamic.html
+ */
+TEST(FollyTest, DynamicTest) {
+    folly::dynamic a = 7;
+    folly::dynamic n = nullptr;
+    folly::dynamic b = true;
+    folly::dynamic f = 1.2;
+    folly::dynamic i = 1;
+    folly::dynamic s = "abc";
+    folly::dynamic arr = folly::dynamic::array(n, i, s, true);
+    folly::dynamic o = folly::dynamic::object(b, f)(f, arr);
+
+    ASSERT_TRUE(a.isInt());
+    ASSERT_TRUE(n.isNull());
+    ASSERT_TRUE(b.isBool());
+    ASSERT_TRUE(f.isDouble());
+    ASSERT_TRUE(i.isInt());
+    ASSERT_TRUE(s.isString());
+    ASSERT_TRUE(arr.isArray());
+    ASSERT_TRUE(o.isObject());
+
+    ASSERT_EQ(folly::dynamic::Type::INT64, a.type());
+
+    ASSERT_EQ(7, a.getInt());
+    ASSERT_EQ(1, arr[1].asInt());
+
+    // asString
+    ASSERT_EQ("7", a.asString());
+
+    // {1.2:[null,1,"abc",true],true:1.2}
+    std::cout << o << std::endl;
+
+    // iterate array
+    std::cout << "array: \n";
+    for (auto item : arr) {
+        std::cout << item << " ";
+    }
+    std::cout << std::endl;
+
+    // iterate object
+    // o.keys, o.values 与 python 很类似
+    std::cout << "object: \n";
+    for (auto [key, value] : o.items()) {
+        std::cout << key << ": " << value << ", ";
+    }
+    std::cout << std::endl;
+}
+
+TEST(FollyTest, JsonTest) {
+    // folly json key must be string
+    const char* test_json = R"({"a": true, "b": 1, "c": [1, 2, 3]})";
+    auto object = folly::parseJson(test_json);
+    ASSERT_TRUE(object.isObject());
+    ASSERT_TRUE(object["a"].asBool());
+    ASSERT_TRUE(object["a"].asBool());
+    ASSERT_TRUE(object["a"].asBool());
+}
