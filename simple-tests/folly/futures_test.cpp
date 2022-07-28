03753cf952b1580a6a77e82522a4c64a0693f974
icejoywoo <icejoywoo@gmail.com>
2022-07-28 22:22:51 +0800

    update

--- simple-tests/folly/futures_test.cpp
+++ simple-tests/folly/futures_test.cpp
@@ -3,6 +3,8 @@
 #include "folly/executors/ThreadedExecutor.h"
 #include "folly/futures/Future.h"
 
+#include <future>
+
 TEST(FollyTest, FutureTest) {
     folly::ThreadedExecutor executor;
     printf("making Promise\n");
@@ -21,4 +23,32 @@ TEST(FollyTest, FutureTest) {
     auto x = std::move(f2).get();
     ASSERT_EQ(42, x);
     printf("Promise fulfilled\n");
+}
+
+TEST(StlTest, FutureTest) {
+    std::future<int> result = std::async([](){
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        return 8;
+    });
+
+    ASSERT_EQ(8, result.get());
+
+    // https://en.cppreference.com/w/cpp/thread/future
+    std::promise<int> p;
+    std::future<int> f = p.get_future();
+
+    std::thread t([&p]{
+        try {
+            // code that may throw
+            throw std::runtime_error("Example");
+        } catch(...) {
+            try {
+                // store anything thrown in the promise
+                p.set_exception(std::current_exception());
+            } catch(...) {} // set_exception() may throw too
+        }
+    });
+
+    ASSERT_ANY_THROW(f.get());
+    t.join();
 }
