8914e7d521e22a54b0c778b45cbeb4a694516f21
icejoywoo <icejoywoo@gmail.com>
2022-07-31 22:13:22 +0800

    update

--- xsimd-demo/xsimd_aligned_bench.cpp
+++ xsimd-demo/xsimd_aligned_bench.cpp
@@ -80,5 +80,28 @@ static void BM_aligned(benchmark::State& state) {
 BENCHMARK(BM_unaligned)->Range(8, 8<<10);
 BENCHMARK(BM_aligned)->Range(8, 8<<10);
 
-// aligned vs unaligned, 差异不大
+/**
+ * mac 上测试差异不大，在 linux 上测试差异很大，aligned 速度明显更快
+ * result on ubuntu 20.04 (16 cores, 64GB):
+Run on (16 X 3500.78 MHz CPU s)
+CPU Caches:
+  L1 Data 32 KiB (x8)
+  L1 Instruction 32 KiB (x8)
+  L2 Unified 1024 KiB (x8)
+  L3 Unified 36608 KiB (x1)
+Load Average: 5.17, 2.69, 5.01
+----------------------------------------------------------------------------
+Benchmark                  Time             CPU   Iterations UserCounters...
+----------------------------------------------------------------------------
+BM_unaligned/8          3.46 ns         3.46 ns    202340130 bytes_per_second=2.15412G/s
+BM_unaligned/64         23.6 ns         23.6 ns     29598115 bytes_per_second=2.52133G/s
+BM_unaligned/512         239 ns          239 ns      2949608 bytes_per_second=1.99378G/s
+BM_unaligned/4096       2069 ns         2069 ns       337888 bytes_per_second=1.84364G/s
+BM_unaligned/8192       4042 ns         4042 ns       174740 bytes_per_second=1.88762G/s
+BM_aligned/8            2.59 ns         2.59 ns    269963641 bytes_per_second=2.87369G/s
+BM_aligned/64           11.7 ns         11.7 ns     59941634 bytes_per_second=5.10527G/s
+BM_aligned/512          91.8 ns         91.8 ns      7625224 bytes_per_second=5.19546G/s
+BM_aligned/4096         1459 ns         1459 ns       479763 bytes_per_second=2.61505G/s
+BM_aligned/8192         2934 ns         2934 ns       238683 bytes_per_second=2.60063G/s
+ */
 BENCHMARK_MAIN();
