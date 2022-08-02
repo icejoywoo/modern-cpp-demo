96f0ba6a1d7a67ebfca61e7d0aae1fc42ea40873
icejoywoo <icejoywoo@gmail.com>
2022-08-02 19:14:42 +0800

    update

--- xsimd-demo/bit_packing_test.cpp
+++ xsimd-demo/bit_packing_test.cpp
@@ -6,6 +6,20 @@ namespace bit::packing {
 
 TEST(BitPackingTest, BasicTest) {
   {
+    uint8_t input[] = {0, 1, 0, 1};
+    uint8_t expected[] = {0b01010000};
+    uint8_t output[1] = {0};
+    Simple::encodeAsBits(input, sizeof(input) / sizeof(char), output);
+    EXPECT_EQ(expected[0], output[0]);
+    PackedStruct::encodeAsBits(input, sizeof(input) / sizeof(char), output);
+    EXPECT_EQ(expected[0], output[0]);
+#ifdef __SSE2__
+    Sse2::encodeAsBits(input, sizeof(input) / sizeof(char), output);
+    EXPECT_EQ(expected[0], output[0]);
+#endif
+  }
+
+  {
     uint8_t input[] = {0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0};
     uint8_t expected[] = {0b01011010, 0b01011010};
     uint8_t output[2] = {0};
@@ -21,6 +35,7 @@ TEST(BitPackingTest, BasicTest) {
     EXPECT_EQ(expected[1], output[1]);
 #endif
   }
+
   {
     uint8_t input[] = {0, 1, 0, 1, 1, 0, 1, 0, 0, 1};
     uint8_t expected[] = {0b01011010, 0b01000000};
