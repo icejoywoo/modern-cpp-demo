69c1a1cc4f16bd9dbfe172a668298c9ada6259b0
icejoywoo <icejoywoo@gmail.com>
2022-08-05 23:30:25 +0800

    update

--- simple-tests/boost/intrusive_ptr_test.cpp
+++ simple-tests/boost/intrusive_ptr_test.cpp
@@ -5,6 +5,64 @@
 #include <atomic>
 #include <iostream>
 
+// velox buffer
+class Buffer {
+ public:
+  virtual ~Buffer() = default;
+
+  void addRef() {
+    referenceCount_.fetch_add(1);
+  }
+
+  void release() {
+    if (referenceCount_.fetch_sub(1) == 1) {
+      releaseResources();
+      delete this;
+    }
+  }
+
+  int refCount() const {
+    return referenceCount_;
+  }
+
+  virtual void releaseResources() {
+    // Overridden in descendants for freeing the data with non-trivial
+    // destructors Note that Buffer's destructor may not be called in case of
+    // pools, so we have to have a separate method.
+  }
+
+ private:
+  std::atomic<int32_t> referenceCount_;
+};
+
+using BufferPtr = boost::intrusive_ptr<Buffer>;
+
+static inline void intrusive_ptr_add_ref(Buffer* buffer) {
+  buffer->addRef();
+}
+
+static inline void intrusive_ptr_release(Buffer* buffer) {
+  buffer->release();
+}
+
+TEST(BoostTest, VeloxBufferPtrTest) {
+  BufferPtr ptr(new Buffer);
+  ASSERT_EQ(1, ptr->refCount());
+  BufferPtr ptr2 = ptr;
+  ASSERT_EQ(2, ptr->refCount());
+  ASSERT_EQ(2, ptr2->refCount());
+  BufferPtr ptr3(ptr.get());
+  ASSERT_EQ(3, ptr->refCount());
+  ASSERT_EQ(3, ptr3->refCount());
+
+  /**
+   * shared_ptr 不可以像下面这样使用，会导致 double free 的问题：
+   * std::shared_ptr<Buffer> a(new Buffer);
+   * std::shared_ptr<Buffer> b(a.get());
+   * intrusive_ptr 是可以这样使用的，引用计数需要自定义设置
+   */
+}
+
 class ReferenceCounter
 {
  public:
