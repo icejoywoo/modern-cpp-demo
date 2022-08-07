bff3d7b300dea487fc5457d31c4ded81006175fd
icejoywoo <icejoywoo@gmail.com>
2022-08-07 12:44:00 +0800

    update

--- simple-tests/poco/http_server.cpp
+++ simple-tests/poco/http_server.cpp
@@ -3,7 +3,7 @@
 #include <Poco/Net/HTTPRequestHandler.h>
 #include <Poco/Net/HTTPRequestHandlerFactory.h>
 #include <Poco/Net/HTTPResponse.h>
-#include <Poco/Net/HttpServerRequest.h>
+#include <Poco/Net/HTTPServerRequest.h>
 #include <Poco/Net/HTTPServerResponse.h>
 #include <Poco/Util/ServerApplication.h>
 #include <Poco/URI.h>
