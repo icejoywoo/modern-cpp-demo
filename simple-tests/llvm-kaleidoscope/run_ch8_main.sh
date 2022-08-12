142728769c272c335b6ac5fa7df3749ad9cd0ab5
icejoywoo <icejoywoo@gmail.com>
2022-08-12 23:21:16 +0800

    update

--- simple-tests/llvm-kaleidoscope/run_ch8_main.sh
+++ simple-tests/llvm-kaleidoscope/run_ch8_main.sh
@@ -5,5 +5,13 @@
 # ^D
 # Wrote output.o
 
-g++ ch8_main.cpp ../../_build/output.o -o main && ./main && rm ./main
+BUILD_DIR=../../_build/
+
+${BUILD_DIR}/simple-tests/llvm-kaleidoscope/kaleidoscope_ch8 < average.txt > /dev/null 2>&1
+
+g++ ch8_main.cpp output.o -o main && ./main
 # average of 3.0 and 4.0: 3.5
+
+# clean
+
+rm ./main output.o
